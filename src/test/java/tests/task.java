e73a802fbe8cf8d6a67027ff35015bd839e89bd9
MustafaOguzhan <56677856+MustafaOguzhan@users.noreply.github.com>
2019-12-09 18:39:03 +0100

    updated task class over github

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -1,4 +1,11 @@
 package tests;
 
 public class task {
+  
+  public static void main(String [] arg){
+  System.out.println("Hello,we r in github now);
+                     //we add thi sline from github
+  
+  }
+  
 }
